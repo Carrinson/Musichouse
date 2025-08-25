9af83b399cc6fb1ad5ba3d4fa7bc04443d1da5ee
Carrinson <125251931+Carrinson@users.noreply.github.com>
2025-08-25 14:33:57 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import {
   Row,
 } from "react-bootstrap";
 import { useState, useEffect } from "react";
-import Navbar from "./components/navbar"; 
+import Navbar from "./components/Navbar"; 
 import Homepage from "./pages/Homepage";
 import Browse from "./pages/Browse";
 import { Links, Routes, Route } from "react-router-dom";
